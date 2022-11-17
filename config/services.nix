c873f27a8d5016d4d7c03c5517d3e4cfccb09f00
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2022-11-17 15:16:23 +0100

    chore: adds services.nix

---
+++ config/services.nix
@@ -0,0 +1,37 @@
+let
+  nixpkgs = import <nixpkgs-22.05-darwin> {};
+  postgresql = "postgresql_14";
+in
+  nixpkgs.mkShell {
+    buildInputs = [
+      nixpkgs.hivemind
+      nixpkgs.${postgresql}
+      nixpkgs.redis
+    ];
+
+    shellHook = ''
+      export DATA=$PWD/data
+      mkdir -p $DATA
+
+      # Redis Configuration
+      mkdir -p $DATA/redis
+      cat << EOF > $DATA/redis/redis.conf
+      loglevel warning
+      logfile ""
+      dir ./data/redis
+      EOF
+
+      # PostgreSQL Configuration
+      export PGDATA=$DATA/${postgresql}
+      if [[ ! -d "$PGDATA" ]]; then
+        initdb --auth=trust --no-locale --encoding=UTF8
+        echo "CREATE DATABASE $USER;" | postgres --single -E postgres
+      fi
+
+      # Create Procfile
+      cat << EOF > Procfile
+      postgresql: postgres -D \$PGDATA
+      redis: redis-server \$DATA/redis/redis.conf
+      EOF
+    '';
+  }
