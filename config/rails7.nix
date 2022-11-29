265ac23f2e6718425b9564d47f2f4e0b47d93013
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2022-11-29 18:20:25 +0100

    chore: nodejs and yarn

--- config/rails7.nix
+++ config/rails7.nix
@@ -2,10 +2,12 @@ with (import <nixpkgs-22.11-darwin> {});
 mkShell {
   buildInputs = [
     cmake
+    nodejs-16_x
     openssl
     pkg-config
     rubyPackages_3_1.rugged
     ruby_3_1
+    yarn
   ];
   shellHook = ''
     export GEM_HOME=$PWD/.nix-gems
