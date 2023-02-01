48d7a3567f4be2216d41cfc54387242f27d3b021
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2023-02-01 23:30:12 +0100

    chore: some package updates

--- env.nix
+++ env.nix
@@ -1,8 +1,13 @@
 let
   nixpkgs = import <nixpkgs-22.11-darwin> {};
-  nixpkgsUnstable = import <nixpkgs> {};
+  nixpkgsUnstable = import <nixpkgs-unstable> {};
 in [
+  # nixpkgs.python3Full
+  # nixpkgs.riot-redis
+  # nixpkgs.tree
+  # nixpkgs.virtualenv
   nixpkgs.ack
+  nixpkgs.asdf-vm
   nixpkgs.awscli2
   nixpkgs.awslogs
   nixpkgs.backblaze-b2
@@ -13,6 +18,7 @@ in [
   nixpkgs.ctags
   nixpkgs.direnv
   nixpkgs.docker-client
+  nixpkgs.exa
   nixpkgs.fzf
   nixpkgs.gh
   nixpkgs.git
@@ -20,33 +26,34 @@ in [
   nixpkgs.git-extras
   nixpkgs.gnupg
   nixpkgs.gnused
-  nixpkgs.go_1_18
+  nixpkgs.go
   nixpkgs.hivemind
   nixpkgs.htop
   nixpkgs.imagemagick
   nixpkgs.jetbrains-mono
   nixpkgs.jq
+  nixpkgs.lsd
   nixpkgs.nix-direnv
   nixpkgs.nix-zsh-completions
   nixpkgs.nodejs-18_x
   nixpkgs.openssl
   nixpkgs.pgcli
-  nixpkgs.postgresql_14
+  nixpkgs.pkg-config
+  nixpkgs.postgresql_15
   nixpkgs.pwgen
   nixpkgs.python310
   nixpkgs.python310Packages.pip
+  nixpkgs.rabbitmq-server
   nixpkgs.redis
   nixpkgs.restic
-  nixpkgs.riot-redis
   nixpkgs.ripgrep
   nixpkgs.rsync
+  nixpkgs.rubyPackages_3_1.openssl
   nixpkgs.ruby_3_1
   nixpkgs.shellcheck
   nixpkgs.stow
   nixpkgs.terraform
-  nixpkgs.tree
   nixpkgs.vim
-  nixpkgs.virtualenv
   nixpkgs.wget
   nixpkgs.yarn
   nixpkgs.zsh
