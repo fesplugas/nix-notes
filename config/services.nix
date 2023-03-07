1360e16f5641f45316bdfe121030c59195489ed4
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2023-03-07 12:37:05 +0100

    chore: simplify services.nix

--- config/services.nix
+++ config/services.nix
@@ -1,37 +1,34 @@
-let
-  nixpkgs = import <nixpkgs-22.11-darwin> {};
-  postgresql = "postgresql_14";
-in
-  nixpkgs.mkShell {
-    buildInputs = [
-      nixpkgs.hivemind
-      nixpkgs.${postgresql}
-      nixpkgs.redis
-    ];
+with (import <nixpkgs-22.11-darwin> {});
+mkShell {
+  buildInputs = [
+    hivemind
+    postgresql_14
+    redis
+  ];
 
-    shellHook = ''
-      export DATA=$PWD/data
-      mkdir -p $DATA
+  shellHook = ''
+    export DATA=$PWD/data
+    mkdir -p $DATA
 
-      # Redis Configuration
-      mkdir -p $DATA/redis
-      cat << EOF > $DATA/redis/redis.conf
-      loglevel warning
-      logfile ""
-      dir ./data/redis
-      EOF
+    # Redis Configuration
+    mkdir -p $DATA/redis
+    cat << EOF > $DATA/redis/redis.conf
+    loglevel warning
+    logfile ""
+    dir ./data/redis
+    EOF
 
-      # PostgreSQL Configuration
-      export PGDATA=$DATA/${postgresql}
-      if [[ ! -d "$PGDATA" ]]; then
-        initdb --auth=trust --no-locale --encoding=UTF8
-        echo "CREATE DATABASE $USER;" | postgres --single -E postgres
-      fi
+    # PostgreSQL Configuration
+    export PGDATA=$DATA/postgresql
+    if [[ ! -d "$PGDATA" ]]; then
+      initdb --auth=trust --no-locale --encoding=UTF8
+      echo "CREATE DATABASE $USER;" | postgres --single -E postgres
+    fi
 
-      # Create Procfile
-      cat << EOF > Procfile
-      postgresql: postgres -D \$PGDATA
-      redis: redis-server \$DATA/redis/redis.conf
-      EOF
-    '';
-  }
+    # Create Procfile
+    cat << EOF > Procfile
+    postgresql: postgres -D \$PGDATA
+    redis: redis-server \$DATA/redis/redis.conf
+    EOF
+  '';
+}
