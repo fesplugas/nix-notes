3a5b81fbad317598650959a991ce557a066c7265
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2023-06-17 00:45:26 +0200

    chore: example with tarball and script

---
+++ config/example.nix
@@ -0,0 +1,19 @@
+# { pkgs ? import <nixpkgs-23.05> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/nixos/nixpkgs/archive/nixpkgs-21.05-darwin.tar.gz") {} }:
+
+let fooScript = pkgs.writeScriptBin "foo.sh" ''
+  #!/bin/sh
+  echo $FOO
+'';
+
+in pkgs.mkShell {
+  name = "My-project build environment";
+  buildInputs = [
+    pkgs.hello
+    fooScript
+  ];
+  shellHook = ''
+    echo "Welcome in $name"
+    export FOO="BAR"
+  '';
+}
