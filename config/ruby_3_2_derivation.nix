14ffbfd29e8af0c93ecb3f07fbaa0b4e04b0756c
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2023-06-17 00:44:25 +0200

    chore: attempt to install ruby 3.2 + openssl 1.1.1

---
+++ config/ruby_3_2_derivation.nix
@@ -0,0 +1,30 @@
+with (import <nixpkgs-23.05> {});
+let
+  myOpenSSL = openssl_1_1.override {
+    # Add any custom OpenSSL configuration options here
+  };
+  myRuby = ruby_3_2.overrideAttrs (oldAttrs: {
+    # Set the OpenSSL package to the custom version defined above
+    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ myOpenSSL ];
+    buildInputs = oldAttrs.buildInputs ++ [ myOpenSSL ];
+    # Disable the postUnpack function
+    postUnpack = "";
+  });
+in
+mkShell {
+  buildInputs = [
+    cmake
+    nodejs_18
+    pkg-config
+    yarn
+    myRuby
+  ];
+
+  shellHook = ''
+    export GEM_HOME=$HOME/.local/share/gem/ruby/3.2.0
+    export GEM_PATH=$GEM_HOME
+    export PATH=$GEM_HOME/bin:$PATH
+
+    gem install bundler --version="~> 2.4.0" --no-document --conservative
+  '';
+}
