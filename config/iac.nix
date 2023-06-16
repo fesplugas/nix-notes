c364fafb33ae81fd2740b85055e7886fcea1a900
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2023-06-17 00:42:25 +0200

    chore: updates iac.nix
    Removes shellHook and updates ruby to 3.2

--- config/iac.nix
+++ config/iac.nix
@@ -1,19 +1,13 @@
-with (import <nixpkgs-22.11> {});
+with (import <nixpkgs-23.05> {});
 
 mkShell {
   buildInputs = [
     awscli2
     jq
     packer
-    ruby_3_1
+    ruby_3_2
     terraform
     terraform-docs
     terragrunt
   ];
-
-  shellHook = ''
-    export GEM_HOME=$HOME/.local/share/gem/ruby/3.1.0-nix
-    export GEM_PATH=$GEM_HOME
-    export PATH=$GEM_HOME/bin:$PATH
-  '';
 }
