df56e039eb3dba63c6a352e4373547091212c699
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2023-08-02 20:11:37 +0200

    fix: O_o

--- config/ruby_3_2.nix
+++ config/ruby_3_2.nix
@@ -14,7 +14,6 @@ mkShell {
     export GEM_HOME=$HOME/.local/share/gem/ruby/3.2.0
     export GEM_PATH=$GEM_HOME
     export PATH=./bin:$GEM_HOME/bin:$PATH
-    export PATH=$HOME/Code/getdreams/dreams-iac/toolbox/bin:$PATH
 
     gem install bundler --version="~> 2.4.0" --no-document --conservative
   '';
