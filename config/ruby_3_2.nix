2ac118ccb6932a16c83c557bf5ad2aa653ce2285
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2023-08-29 15:54:07 +0200

    chore: updates ruby configuration

--- config/ruby_3_2.nix
+++ config/ruby_3_2.nix
@@ -1,20 +1,21 @@
-with (import (fetchTarball https://github.com/nixos/nixpkgs/archive/nixpkgs-23.05-darwin.tar.gz) {});
+let
+  # version = "nixpkgs-23.05-darwin";
+  version = "c540061ac8d72d6e6d99345bd2d590c82b2f58c1";
+  pkgs = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/${version}.tar.gz") {};
+in
+  pkgs.mkShell {
+    packages = [
+      pkgs.cmake
+      pkgs.nodejs_18
+      pkgs.openssl
+      pkgs.pkg-config
+      pkgs.ruby_3_2
+      pkgs.yarn
+    ];
 
-mkShell {
-  buildInputs = [
-    cmake
-    nodejs_18
-    openssl
-    pkg-config
-    ruby_3_2
-    yarn
-  ];
-
-  shellHook = ''
-    export GEM_HOME=$HOME/.local/share/gem/ruby/3.2.0
-    export GEM_PATH=$GEM_HOME
-    export PATH=./bin:$GEM_HOME/bin:$PATH
-
-    gem install bundler --version="~> 2.4.0" --no-document --conservative
-  '';
-}
+    _shellHook = ''
+      export GEM_HOME=$HOME/.local/share/gem/ruby/3.2.0
+      export GEM_PATH=$GEM_HOME
+      export PATH=$GEM_HOME/bin:$PATH
+    '';
+  }
