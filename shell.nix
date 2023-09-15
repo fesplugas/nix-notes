51c7332caa70473ba8ca39e4d09ad75431dadbec
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2023-09-15 17:43:17 +0200

    chore: updates shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,14 @@
-with (import <nixpkgs> {});
-mkShell {
-  buildInputs = [
-    hello
-  ];
-}
+let
+  version = "nixpkgs-unstable";
+  pkgs = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/${version}.tar.gz") {};
+in
+  pkgs.mkShell {
+    name = "hello-world";
+    packages = [
+      pkgs.hello
+    ];
+
+    shellHook = ''
+      echo "You are using a nix-shell!"
+    '';
+  }
