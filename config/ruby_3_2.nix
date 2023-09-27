59a75be679719a38ad598c59d1b2523dec735b19
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2023-09-27 22:45:45 +0200

    chore: wip

--- config/ruby_3_2.nix
+++ config/ruby_3_2.nix
@@ -1,6 +1,6 @@
 let
-  # version = "nixpkgs-23.05-darwin";
-  version = "e27ca312d56522b907b998c2ff99169bf12639f2";
+  version = "nixpkgs-23.05-darwin";
+  # version = "e27ca312d56522b907b998c2ff99169bf12639f2";
   pkgs = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/${version}.tar.gz") {};
 in
   pkgs.mkShell {
@@ -12,10 +12,4 @@ in
       pkgs.ruby_3_2
       pkgs.yarn
     ];
-
-    _shellHook = ''
-      export GEM_HOME=$HOME/.local/share/gem/ruby/3.2.0
-      export GEM_PATH=$GEM_HOME
-      export PATH=$GEM_HOME/bin:$PATH
-    '';
   }
