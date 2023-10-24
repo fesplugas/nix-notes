b4da1c6a3bad10c745e953fafb9453e5518150cf
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2023-10-24 18:03:32 +0200

    chore: updates example.nix

--- config/example.nix
+++ config/example.nix
@@ -1,19 +1,20 @@
-# { pkgs ? import <nixpkgs-23.05> {} }:
-{ pkgs ? import (fetchTarball "https://github.com/nixos/nixpkgs/archive/nixpkgs-23.05-darwin.tar.gz") {} }:
+let
+  version = "nixpkgs-23.05-darwin";
+  pkgs = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/${version}.tar.gz") {};
 
-let fooScript = pkgs.writeScriptBin "foo.sh" ''
-  #!/bin/sh
-  echo $FOO
-'';
-
-in pkgs.mkShell {
-  name = "My-project build environment";
-  buildInputs = [
-    pkgs.hello
-    fooScript
-  ];
-  shellHook = ''
-    echo "Welcome in $name"
-    export FOO="BAR"
-  '';
-}
+  fooScript = pkgs.writeScriptBin "foo.sh" ''
+    #!/bin/sh
+    echo $FOO
+    '';
+in
+  pkgs.mkShell {
+    name = "My-project build environment";
+    buildInputs = [
+      pkgs.hello
+      fooScript
+    ];
+    shellHook = ''
+      echo "Welcome in $name"
+      export FOO="bar"
+    '';
+  }
