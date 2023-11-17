5b206f5734bb6c3580a3b9df83e5012aa8ce5936
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2023-11-17 13:12:41 +0100

    chore: updates ruby_3_2.nix

--- config/ruby_3_2.nix
+++ config/ruby_3_2.nix
@@ -1,28 +1,35 @@
 let
-  version = "nixpkgs-23.05-darwin";
+  # version = "nixpkgs-23.05-darwin";
+  version = "nixpkgs-unstable";
   pkgs = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/${version}.tar.gz") {};
 in
   pkgs.mkShell {
-    packages = with pkgs; [
-      cmake
-      foreman
-      gh
-      git
-      hivemind
-      lefthook
-      libyaml
-      nodejs_18
-      openssl
-      pkg-config
-      postgresql_15
-      ruby_3_2
-      yarn
+    name = "ruby-3.2";
+
+    packages = [
+      pkgs.gh
+      pkgs.git
+      pkgs.hivemind
+      pkgs.lefthook
+      pkgs.nodejs_18
+      pkgs.postgresql_15
+      pkgs.ruby_3_2
+      pkgs.yarn
+    ];
+
+    inputsFrom = [
+      pkgs.cmake
+      pkgs.libyaml
+      pkgs.openssl
+      pkgs.pkg-config
+      pkgs.postgresql_15
     ];
 
     # If you are on Linux you might need to set this
     # PGHOST="/tmp";
 
     shellHook = ''
+      echo "You are using a nix-shell (enabled with direnv)"
       export PATH=$(gem env home)/bin:$PATH
       export PATH=./bin:$PATH
     '';
